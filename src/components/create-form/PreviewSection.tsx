c659e2cdbef658888be2e2c509121c14ea8b3c74
Pierre Bonnin <pierre.bonnin@interstellarlab.earth>
2025-03-27 10:39:46 +0100

    Add explanation text for each editor view

--- src/components/create-form/PreviewSection.tsx
+++ src/components/create-form/PreviewSection.tsx
@@ -47,7 +47,74 @@ function PreviewSection({
 
 
   return (
-    <div className="flex-auto flex flex-col p-3 md:p-6 gap-6 z-10">
+    <div className="flex-auto flex flex-col p-6 gap-6 z-10">
+      {currentView === 'calendar' && (
+        <div 
+          className="flex"
+        >
+          <div className="flex flex-col gap-3 px-6 py-4 bg-neutral-100 border border-neutral-50 rounded-lg mx-auto max-w-sm">
+            <div className="flex items-center gap-1">
+              <span className="material-symbols-rounded">palette</span>
+              <span className="text-sm ml-1 font-medium">Apparence</span>
+            </div>
+            <p className="text-sm text-zinc-500">
+              Choisissez une photo de fond et personnalisez l&apos;apparence de votre calendrier.
+            </p>
+          </div>
+        </div>
+      )}
+      {currentView === 'stickers' && (
+        <div className="flex">
+          <div className="flex flex-col gap-3 px-6 py-4 bg-neutral-100 border border-neutral-50 rounded-lg mx-auto max-w-sm">
+            <div className="flex items-center gap-1">
+              <span className="material-symbols-rounded">apps</span>
+              <span className="text-sm ml-1 font-medium">Gommettes</span>
+            </div>
+            <p className="text-sm text-zinc-500">
+              Ajoutez une image au héros du calendrier puis créez les activités qu&apos;il devra affronter dans sa semaine.
+            </p>
+            <p className="text-zinc-500 flex items-start gap-1">
+              <span className="flex-none material-symbols-rounded !text-[18px]/[18px]">info</span>
+              <span className="text-xs">
+                Concentrez-vous sur les moments importants et/ou qui vous semblent bloquants pour votre enfant.
+              </span>
+            </p>
+            <p className="text-zinc-500 flex items-start gap-1">
+              <span className="flex-none material-symbols-rounded !text-[18px]/[18px]">content_cut</span>
+              <span className="text-xs">
+                Une fois imprimées et découpées, les gommettes vous serviront à planifier avec votre enfant sa semaine.
+              </span>
+            </p>
+            <p className="text-zinc-500 flex items-start gap-1">
+              <span className="flex-none material-symbols-rounded !text-[18px]/[18px]">pan_tool_alt</span>
+              <span className="text-xs">
+                Chaque jour, votre enfant pourra déplacer sa gommette sur la bonne colonne pour visualiser les étapes de sa journée.
+              </span>
+            </p>
+          </div>
+        </div>
+      )}
+      {currentView === 'all' && (
+        <div 
+          className="flex"
+        >
+          <div className="flex flex-col gap-3 px-6 py-4 bg-neutral-100 border border-neutral-50 rounded-lg mx-auto max-w-sm">
+            <div className="flex items-center gap-1">
+              <span className="material-symbols-rounded">export_notes</span>
+              <span className="text-sm ml-1 font-medium">Exportation</span>
+            </div>
+            <p className="text-sm text-zinc-500">
+              Téléchargez votre calendrier en PDF pour l&apos;imprimer et l&apos;utiliser.
+            </p>
+            <p className="text-xs text-zinc-500 flex items-start gap-1">
+              <span className="flex-none material-symbols-rounded !text-[18px]/[18px]">save</span>
+              <span className="text-xs">
+                Vous pouvez aussi l&apos;exporter pour pouvoir l&apos;utiliser sur un autre appareil.
+              </span>
+            </p>
+          </div>
+        </div>
+      )}
       {/* Preview content */}
       <div className="space-y-8">
         <motion.div
